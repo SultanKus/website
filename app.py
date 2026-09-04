f9decb42b9ff5dd78d5587b94dce15354b3eb178
Sultan <138854194+SultanKus@users.noreply.github.com>
2026-09-04 12:31:29 +0300

    Remove commented FontAwesome library line

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ st.set_page_config(
 )
 
 # FontAwesome İkon Kütüphanesi ve CSS
-# FontAwesome İkon Kütüphanesi ve CSS
 st.markdown("""
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
